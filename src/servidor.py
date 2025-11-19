27bd36df1e70695adf7727ae14d28d81000475b3
Matheus Araújo Akiyoshi Loureiro <84807068+MatheusLoureiro2003@users.noreply.github.com>
2025-11-19 19:09:47 +0000

    importando a hashlib, para calcular SHA256, implementando a função pra fazer isso e outra função de documento não encontrado.

--- src/servidor.py
+++ src/servidor.py
@@ -1,9 +1,30 @@
 import socket
 import threading
+import hashlib #necessario pa calcular o SHA-256
+import os # Necessário para checar a existência e tamanho do arquivo
 
 # Configurações do Servidor
 HOST = '127.0.0.1' 
 PORTA = 12345       # Porta TCP para escutar (acima de 1024)
+BUFFER_SIZE = 1024 # Tamanho padrão de buffer
+
+# Diretório onde os arquivos de teste estão localizados
+FILE_STORAGE_DIR = "server_files"
+
+def calcula_sha256(filepath):
+    """Calcula o hash SHA256 de um aquivo em chuncks."""
+    sha256_hash = hashlib.sha256()
+    with open(filepath, "rb") as f:
+        for chunk in iter(lambda: f.read(4096),b""):
+            sha256_hash.update(chunk)
+    return sha256_hash.hexdigest()
+
+def send_file_not_found(conn, filename):
+    """Envia um status de erro ao cliente."""
+    # Protocolo: STATUS_ERROR(0) Nome_do_Arquivo
+    error_message = f"0 {filename} Arquivo não encontrado."
+    conn.sendall(error_message.encode('utf-8'))
+    print(f"❌ [CLIENTE {conn.getpeername()}] Erro: Arquivo '{filename}' não encontrado.")
 
 def handle_client(conn, addr):
     """
